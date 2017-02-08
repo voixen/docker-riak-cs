bc0df18ce74ad53e092baeeee208581bcf81e5a6
David Rohmer <rohmer.david@gmail.com>
2017-02-08 16:48:40 +0100

    fix and update riak config patching

--- src/script/patch_configuration.sh
+++ src/script/patch_configuration.sh
@@ -6,35 +6,43 @@
 function riak_patch_config(){
     echo -n 'Updating Riak configuration…'
     local advancedConfigPath='/etc/riak/advanced.config'
+    local riakConfigPath='/etc/riak/riak.conf'
+    
+    
+    sed -i '/storage_backend = bitcask/d'         $riakConfigPath
+    echo -e "\nbuckets.default.allow_mult = true" >> $riakConfigPath
+    echo    "javascript.map_pool_size = 0"        >> $riakConfigPath
+    echo    "javascript.red   uce_pool_size = 0"  >> $riakConfigPath
+    echo    "javascript.hook_pool_size = 0"       >> $riakConfigPath
 
     # Expose the necessary Riak CS modules to Riak and instruct Riak to use the custom backend provided by Riak CS. Set
     # the `allow_mult` parameter to `true` to enable Riak to create siblings, which is necessary for Riak CS to function.
 
     cat <<-EOL > $advancedConfigPath
-		[
-		    {riak_kv, [
-		        %% Storage_backend specifies the Erlang module defining the storage mechanism that will be used on this node.
-		        {add_paths, ["$(ls -d /usr/lib/riak-cs/lib/riak_cs-*)/ebin"]},
-		        {storage_backend, riak_cs_kv_multi_backend},
-		        {multi_backend_prefix_list, [{<<"0b:">>, be_blocks}]},
-		        {multi_backend_default, be_default},
-		        {multi_backend, [
-		            {be_default, riak_kv_eleveldb_backend, [
-		                {max_open_files, 30},
-		                {data_root, "/var/lib/riak/leveldb"}
-		            ]},
-		            {be_blocks, riak_kv_bitcask_backend, [
-		                {data_root, "/var/lib/riak/bitcask"}
-		            ]}
-		        ]}
-		    ]},
-		    {riak_core, [
-		        {default_bucket_props, [{allow_mult, true}]}
-		    ]}
-		].
-	EOL
+        [
+            {eleveldb, [
+                {total_leveldb_mem_percent, 30}
+            ]},
+            {riak_kv, [
+                %% Storage_backend specifies the Erlang module defining the storage mechanism that will be used on this node.
+                {add_paths, ["$(ls -d /usr/lib/riak-cs/lib/riak_cs-*)/ebin"]},
+                {storage_backend, riak_cs_kv_multi_backend},
+                {multi_backend_prefix_list, [{<<"0b:">>, be_blocks}]},
+                {multi_backend_default, be_default},
+                {multi_backend, [
+                    {be_default, riak_kv_eleveldb_backend, [
+                        {max_open_files, 30},
+                        {data_root, "/var/lib/riak/leveldb"}
+                    ]},
+                    {be_blocks, riak_kv_bitcask_backend, [
+                        {data_root, "/var/lib/riak/bitcask"}
+                    ]}
+                ]}
+            ]}
+        ].
+    EOL
 
-	echo ' OK!'
+    echo ' OK!'
 }
 
 function riak_cs_patch_config(){
@@ -47,17 +55,17 @@ function riak_cs_patch_config(){
     # Fixme: ssl currently doesn't work, check back on http://git.io/RxYPrw and update SSL config and user creation URL…
 
     cat <<-EOL > $advancedConfigPath
-		[
-		    {riak_cs, [
-		        {anonymous_user_creation, true},
-		        %%{admin_key, null},
-		        %%{admin_secret, null},
-		        {cs_root_host, "s3.amazonaws.dev"},
-		        {fold_objects_for_list_keys, true},
-		        {listener, {"0.0.0.0", 8080}}
-		    ]}
-		].
-	EOL
+        [
+            {riak_cs, [
+                {anonymous_user_creation, true},
+                %%{admin_key, null},
+                %%{admin_secret, null},
+                {cs_root_host, "s3.amazonaws.dev"},
+                {fold_objects_for_list_keys, true},
+                {listener, {"0.0.0.0", 8080}}
+            ]}
+        ].
+    EOL
 
     echo ' OK!'
 }
@@ -67,15 +75,15 @@ function stanchion_patch_config(){
     local advancedConfigPath='/etc/stanchion/advanced.config'
 
     cat <<-EOL > $advancedConfigPath
-		[
-		    {stanchion, [
-		        %%{admin_key, null},
-		        %%{admin_secret, null}
-		    ]}
-		].
-	EOL
+        [
+            {stanchion, [
+                %%{admin_key, null},
+                %%{admin_secret, null}
+            ]}
+        ].
+    EOL
 
-	echo ' OK!'
+    echo ' OK!'
 }
 
 riak_patch_config
